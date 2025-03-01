2390ae7e31b918bbea6488d0efd904f364351601
YOLO105 <mcpe.amorman@gmail.com>
2025-03-01 23:44:52 +0100

    Updated batch script
    Added automatic install of dependencies if not present.

--- start.bat
+++ start.bat
@@ -1,2 +1,13 @@
-node index.js
+@echo off
+cd /d "%~dp0"
+
+if exist "node_modules\" (
+    node index.js
+) else (
+    echo First startup detected, installing dependencies...
+    npm install
+    cls
+    node index.js
+)
+
 pause
