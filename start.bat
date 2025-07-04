8a4873e595b6b8b3e0c75bae601ae910c2bce550
JoshAtticus <git@joshatticus.site>
2025-07-04 19:01:51 +0800

    undo python rewrite

--- start.bat
+++ start.bat
@@ -1,14 +1,16 @@
 @echo off
 cd /d "%~dp0"
 
-where python3 >nul 2>nul
-if %errorlevel% neq 0 (
-    echo python3 is not installed or not in your PATH.
-    echo Please install Python 3. Opening the Microsoft Store page...
-    start "" "https://apps.microsoft.com/detail/9PNRBTZXMB4Z"
-    pause
-    exit /b
+if exist "node_modules\" (
+    start http://127.0.0.1:3000
+    cls
+    node index.js
+) else (
+    echo First startup detected, installing dependencies...
+    npm install
+    start http://127.0.0.1:3000
+    cls
+    node index.js
 )
 
-python3 main.py
 pause
